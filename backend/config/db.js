5efc803f6dcd9ca46ec437584f2f72fd57ee5616
Raju Katala <95536649+rajukatala@users.noreply.github.com>
2022-09-08 15:53:12 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect("mongodb+srv://rkatala07:Raju@9024@connect.yhebg.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useCreateIndex:true,
       useUnifiedTopology: true,
